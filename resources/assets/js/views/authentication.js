98e455d0143ccb76d83f8a8bc7cee9469a211994
Aljo Victor Gabot <aljo.gabot@gmail.com>
2015-09-18 06:24:16 +0800

    Moved login.js -> authentication.js

---
+++ resources/assets/js/views/authentication.js
@@ -0,0 +1,109 @@
+function AuthenticationPage() {
+	this.construct();
+};
+
+AuthenticationPage.prototype = {
+
+	construct : function() {
+		this.init_login_form();
+		this.init_register_form();
+		this.init_events();
+	},
+
+	init_events : function() {
+
+		$( '.go-to-registration' ).click(
+			function() {
+				$( '#signin-container' ).hide();
+				$( '#register-container' ).show();
+			}
+		);
+
+		$( '.go-to-signin' ).click(
+			function() {
+				$( '#register-container' ).hide();
+				$( '#signin-container' ).show();
+			}
+		);
+
+	},
+
+	init_login_form : function() {
+
+		$( 'form[name=login-form]' ).submit(
+			function( $event ) {
+				$event.preventDefault();
+
+				var $form   = $( this );
+				var $data 	= $form.serialize();
+				var $url	= $form.attr( 'action' );
+
+				$FormMessageService.setElement( $form );
+				$FormMessageService.notify( 'Processing ...' );
+
+				$.ajax(
+					{
+						url	: $url,
+						type: 'POST',
+						dataType: 'json',
+						data: $data,
+						success: function( $json_response ) {
+							if( $json_response.success ) {
+								$FormMessageService.success( $json_response.message );
+
+							} else {
+								$FormMessageService.error( $json_response.message );
+							}
+						}
+					}
+				);
+
+			}
+		);
+
+	},
+
+	init_register_form : function() {
+
+		$( 'form[name=register-form]' ).submit(
+			function( $event ) {
+				$event.preventDefault();
+
+				var $form   = $( this );
+				var $data 	= $form.serialize();
+				var $url	= $form.attr( 'action' );
+
+				$FormMessageService.setElement( $form );
+				$FormMessageService.notify( 'Processing ...' );
+
+				$.ajax(
+					{
+						url	: $url,
+						type: 'POST',
+						dataType: 'json',
+						data: $data,
+						success: function( $json_response ) {
+							if( $json_response.success ) {
+								$FormMessageService.success( $json_response.message );
+
+							} else {
+								$FormMessageService.error( $json_response.message );
+							}
+						}
+					}
+				);
+
+			}
+		);
+
+	}
+
+};
+
+var $AuthenticationPage;
+
+$( document ).ready(
+	function() {
+		$AuthenticationPage = new AuthenticationPage();
+	}
+);
