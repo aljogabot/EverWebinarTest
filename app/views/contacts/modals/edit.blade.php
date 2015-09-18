143e996f1abc055ff0530586c7fed6f296bef772
Aljo Victor Gabot <aljo.gabot@gmail.com>
2015-09-18 12:29:20 +0800

    Updated Contact Form

--- app/views/contacts/modals/edit.blade.php
+++ app/views/contacts/modals/edit.blade.php
@@ -7,10 +7,47 @@
 	    <div class="modal-body">
 	    	<div class="alert" style="display:none;"></div>
 
+	    	<div class="form-group">
+				<label for="name">Name</label>
+		        <input type="text" id="name" name="name" class="form-control" placeholder="Name" required autofocus value="{{ $contact->name }}">
+			</div>
+
 			<div class="form-group">
 				<label for="email">Email Address</label>
 		        <input type="email" id="email" name="email" class="form-control" placeholder="Email Address" required autofocus value="{{ $contact->email }}">
 			</div>
+
+			<div class="form-group">
+				<label for="phone">Phone</label>
+		        <input type="text" id="phone" name="phone" class="form-control" placeholder="Phone" required autofocus value="{{ $contact->phone }}">
+			</div>
+
+			<button type="button" class="btn btn-primary">Show Hide Fields</button>
+
+			<div id="custom-fields-container" style="display: none;">
+
+				<div class="form-group">
+					<input type="text" id="custom_1" name="custom_1" class="form-control" placeholder="" required autofocus value="{{ $contact->custom_1 }}">
+				</div>
+
+				<div class="form-group">
+					<input type="text" id="custom_2" name="custom_2" class="form-control" placeholder="" required autofocus value="{{ $contact->custom_2 }}">
+				</div>
+
+				<div class="form-group">
+					<input type="text" id="custom_3" name="custom_3" class="form-control" placeholder="" required autofocus value="{{ $contact->custom_3 }}">
+				</div>
+
+				<div class="form-group">
+					<input type="text" id="custom_4" name="custom_4" class="form-control" placeholder="" required autofocus value="{{ $contact->custom_4 }}">
+				</div>
+
+				<div class="form-group">
+					<input type="text" id="custom_5" name="custom_5" class="form-control" placeholder="" required autofocus value="{{ $contact->custom_5 }}">
+				</div>
+
+			</div>
+
 	    </div>
 	    <div class="modal-footer">
 	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
