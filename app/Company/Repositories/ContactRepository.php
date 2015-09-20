b06a3be53cadd956e6a4623ff7fbe7ceeb3078c7
Aljo Victor Gabot <aljo.gabot@gmail.com>
2015-09-20 22:33:52 +0800

    Push the Instantiate Method Up to Parent Class

--- app/Company/Repositories/ContactRepository.php
+++ app/Company/Repositories/ContactRepository.php
@@ -1,49 +1,37 @@
 <?php
 
-	namespace Company\Repositories;
+namespace Company\Repositories;
 
-	use Contact;
+use Contact;
+
+class ContactRepository extends EloquentRepository {
 	
-	class ContactRepository extends EloquentRepository {
-		
-		protected $model;
-	
-		public function __construct( Contact $model ) {
-			$this->model = $model;
-		}
+	protected $model;
 
-		public function getByEmail( $email ) {
-
-			$userObject = $this->model->where( 'email', '=', $email )
-			  			 		->first();
-
-			if( $userObject ) {
-				$this->model = $userObject;
-			}
-
-			return $userObject;
-
-		}
-
-		public function instantiate( $contactId, $fields ) {
-
-			$contact = Contact::find( $contactId );
-
-			if( ! $contact ) {
-				return $contact = new Contact( $fields );
-			}
-
-			return $contact->fill( $fields );
-
-		}
-
-		public function getAllBySearch( $text ) {
-
-			return $this->model->orWhere( 'name', 'LIKE', "%$text%" )
-							->orWhere( 'phone', 'LIKE', "%$text%" )
-							->orWhere( 'email', 'LIKE', "%$text%" )
-							->get();
-
-		}
-	
+	public function __construct( Contact $model ) {
+		$this->model = $model;
 	}
+
+	public function getByEmail( $email ) {
+
+		$userObject = $this->model->where( 'email', '=', $email )
+		  			 		->first();
+
+		if( $userObject ) {
+			$this->model = $userObject;
+		}
+
+		return $userObject;
+
+	}
+
+	public function getAllBySearch( $text ) {
+
+		return $this->model->orWhere( 'name', 'LIKE', "%$text%" )
+						->orWhere( 'phone', 'LIKE', "%$text%" )
+						->orWhere( 'email', 'LIKE', "%$text%" )
+						->get();
+
+	}
+
+}
--- app/Company/Repositories/EloquentRepository.php
+++ app/Company/Repositories/EloquentRepository.php
@@ -1,99 +1,111 @@
 <?php 
 
-	namespace Company\Repositories;
-	
-	use DB;
-	use Crypt;
-	
-	abstract class EloquentRepository {
+namespace Company\Repositories;
 
-		public function id() {
-			return $this->model->id;
+use DB;
+use Crypt;
+
+abstract class EloquentRepository {
+
+	public function id() {
+		return $this->model->id;
+	}
+
+	public function loaded() {
+		return ! is_null( $this->model->id );
+	}
+
+	public function getModel() {
+
+		if( ! $this->loaded() )
+			return FALSE;
+
+		return $this->model;
+		
+	}
+
+	/**
+	 * Set a loaded model on the repository ...
+	 * @param \Eloquent $model
+	 */
+	public function setModel( \Eloquent $model ) {
+
+		if( is_null( $model->id ) ) {
+			dd( 'ERROR' );
 		}
 
-		public function loaded() {
-			return ! is_null( $this->model->id );
-		}
+		$this->model = $model;
 
-		public function getModel() {
-
-			if( ! $this->loaded() )
-				return FALSE;
-
-			return $this->model;
-			
-		}
-
-		/**
-		 * Set a loaded model on the repository ...
-		 * @param \Eloquent $model
-		 */
-		public function setModel( \Eloquent $model ) {
-
-			if( is_null( $model->id ) ) {
-				dd( 'ERROR' );
-			}
-
-			$this->model = $model;
-
-			return $this;
-
-		}
-
-		/**
-		 * Get all
-		 * 
-		 *
-		 * @return object
-		 */
-		public function getAll() {
-			return $this->model->get();
-		}
-		/**
-		 * Get specific object
-		 * 
-		 *
-		 * @return object
-		 */
-		public function getByid( $id ) {
-
-			$recordObject = $this->model->find( $id );
-
-			if( $recordObject )
-				$this->model = $recordObject;
-
-			return $recordObject;
-
-		}
-
-		/**
-		 * Add new
-		 * @return object
-		 */
-		public function create( $input ) {
-			return $this->model->create( $input );
-		}
-
-		/**
-		 * @param  $input Array
-		 * @return Bool
-		 */
-		public function save( $input ) {
-			return $this->model->save( $input );
-		}
-
-		public function findByEncryptedId( $id ) {
-
-			if( ! $id )
-				return FALSE;
-
-			$id = Crypt::decrypt( $id );
-
-			if( ! $id )
-				return FALSE;
-
-			return $this->getByid( $id );
-
-		}
+		return $this;
 
 	}
+
+	/**
+	 * Get all
+	 * 
+	 *
+	 * @return object
+	 */
+	public function getAll() {
+		return $this->model->get();
+	}
+	/**
+	 * Get specific object
+	 * 
+	 *
+	 * @return object
+	 */
+	public function getByid( $id ) {
+
+		$recordObject = $this->model->find( $id );
+
+		if( $recordObject )
+			$this->model = $recordObject;
+
+		return $recordObject;
+
+	}
+
+	/**
+	 * Add new
+	 * @return object
+	 */
+	public function create( $input ) {
+		return $this->model->create( $input );
+	}
+
+	/**
+	 * @param  $input Array
+	 * @return Bool
+	 */
+	public function save( $input ) {
+		return $this->model->save( $input );
+	}
+
+	public function findByEncryptedId( $id ) {
+
+		if( ! $id )
+			return FALSE;
+
+		$id = Crypt::decrypt( $id );
+
+		if( ! $id )
+			return FALSE;
+
+		return $this->getByid( $id );
+
+	}
+
+	public function instantiate( $modelId, $data ) {
+
+		$recordObject = $this->model->find( $modelId );
+
+		if( ! $recordObject ) {
+			return $this->model->fill( $data );
+		}
+
+		return $recordObject->fill( $data );
+
+	}
+
+}
